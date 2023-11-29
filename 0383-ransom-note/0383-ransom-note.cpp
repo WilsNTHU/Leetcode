a4afd7e289a4cc1da24c7e739ce6c6fa9e8be060
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2023-11-29 19:24:44 +0800

    Time: 13 ms (75.16%), Space: 9 MB (85.08%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int len = magazine.size();
-        int len_2 = ransomNote.size();
-        map<char, int> m;
+        int len_1 = ransomNote.size();
+        int len_2 = magazine.size();
         
-        for(int i=0; i<len; i++){
-            if(m[magazine[i]]) m[magazine[i]]++;
-            else m[magazine[i]] = 1;
+        int arr[26];
+        memset(arr, 0, sizeof(arr));
+        
+        if(len_1 > len_2) return false;
+        else{
+            for(int i=0; i<len_2; i++) arr[magazine[i]-'a']++;
+            for(int i=0; i<len_1; i++){
+                if(!arr[ransomNote[i]-'a']) return false;
+                else arr[ransomNote[i]-'a']--;
+            }
+            
+            return true;
         }
-        
-        for(int i=0; i<len_2; i++){
-            if(!m[ransomNote[i]]) return false;
-            else m[ransomNote[i]] --;
-        }
-        
-        return true;
     }
 };
