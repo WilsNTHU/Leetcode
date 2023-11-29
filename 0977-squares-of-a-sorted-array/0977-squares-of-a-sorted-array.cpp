7278004a026281b49ae4735bfe55e593c62b72d7
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2023-11-29 21:14:47 +0800

    Time: 31 ms (28.14%), Space: 27.3 MB (17.59%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> result;
+        
+        for(auto it: nums) result.push_back(it * it);
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
