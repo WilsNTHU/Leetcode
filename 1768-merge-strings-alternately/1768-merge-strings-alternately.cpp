15f1b792152ef58831b43b561a7a68cec4443e9f
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2023-11-29 13:07:57 +0800

    Time: 3 ms (49.33%), Space: 6.6 MB (98.39%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int len_1 = word1.size();
+        int len_2 = word2.size();
+        string result;
+        
+        if(len_1 == len_2){
+            int i=0;
+            for(int k=0; k<len_1; k++){
+                result.push_back(word1[i]);
+                result.push_back(word2[i]);
+                i++;
+            }
+        }
+        else if(len_1 < len_2){
+            int i=0;
+            for(int k=0; k<len_1; k++){
+                result.push_back(word1[i]);
+                result.push_back(word2[i]);
+                i++;
+            }
+            for(; i<len_2; i++){
+                result.push_back(word2[i]);
+            }
+        }
+        else{
+            int i=0;
+            for(int k=0; k<len_2; k++){
+                result.push_back(word1[i]);
+                result.push_back(word2[i]);
+                i++;
+            }
+            for(; i<len_1; i++){
+                result.push_back(word1[i]);
+            }
+        }
+        
+        return result;
+    }
+};
