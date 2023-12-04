a6e51a927e57b417994e04f3d058f5addd9a1eda
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2023-12-04 13:44:36 +0800

    Time: 0 ms, Memory: 6.6 MB - LeetHub

--- implement-strstr/implement-strstr.cpp
+++ implement-strstr/implement-strstr.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int res = haystack.find(needle);
-        if(res != string::npos) return res;
-        else return -1;
+        return haystack.find(needle);
     }
 };
