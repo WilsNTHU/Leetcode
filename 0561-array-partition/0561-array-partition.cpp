bc0d4670a3e27f63de49a4503baac2425ff8f756
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2023-12-05 09:42:53 +0800

    Time: 45 ms (30.27%), Space: 28.6 MB (19.04%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(), nums.end());
         int sum = 0;
         for(int i=0; i<nums.size(); i+=2){
-            sum += (nums[i] < nums[i+1]) ? nums[i] : nums[i+1];
+            sum += nums[i];
         }
         
         return sum;
