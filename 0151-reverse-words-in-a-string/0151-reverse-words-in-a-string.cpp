a86f29abf88c110bbe3d72522d34caadb5274454
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2023-12-07 13:18:13 +0800

    Time: 3 ms (86.14%), Space: 8.5 MB (42.51%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> arr;
+        stringstream ss(s);
+        string temp;
+        while(ss >> temp){
+            arr.push_back(temp);
+        }
+        
+        int left = 0, right = arr.size()-1;
+        string answer;
+        for(auto it = arr.rbegin(); it != arr.rend(); it++){
+            if(it != arr.rbegin()) answer += " ";
+            answer += *it;
+        }
+        
+        return answer;
+    }
+};
