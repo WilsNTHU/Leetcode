b6c3300c62bcd5bfe972a254bbc36ed5052fb796
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2023-12-20 16:42:12 +0800

    Time: 940 ms (49.23%), Space: 373 MB (56.41%) - LeetHub

---
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+        vector<int> result;
+        int n = nums.size();
+        for(auto p: queries){
+            vector<pair<string, int>> table;
+            for(int i=0; i<n; i++){
+                table.emplace_back(nums[i].substr(nums[i].size()-p[1], p[1]), i);
+            }
+            sort(table.begin(), table.end());
+            result.emplace_back(table[p[0]-1].second);
+        }
+        
+        return result;
+    }
+    
+};
