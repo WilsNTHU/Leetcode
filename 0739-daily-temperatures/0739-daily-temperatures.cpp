1c2ecf549aaf882815cf79787cccce3fdb254128
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2023-12-30 15:12:45 +0800

    Time: 389 ms (5.01%), Space: 98.9 MB (83.29%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,12 +3,11 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int len = temperatures.size();
         stack<int> s;
-        vector<int> result(len);
+        vector<int> result(len, 0);
         
         for(int i=len-1; i>=0; i--){
             while(!s.empty() && temperatures[i]>=temperatures[s.top()]) s.pop();
-            if(s.empty()) result[i] = 0;
-            else result[i] = s.top()-i;
+            if(!s.empty()) result[i] = s.top()-i;
             s.push(i);
         }
         
