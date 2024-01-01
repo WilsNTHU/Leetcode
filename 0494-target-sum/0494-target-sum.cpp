a8935bd9236497b35c07c528ce72e0e5d64bf770
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-01 17:28:37 +0800

    Time: 290 ms (38.04%), Space: 62 MB (17.01%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,15 +2,20 @@ class Solution {
 public:
     
     int findTargetSumWays(vector<int>& nums, int target) {
-        return DFS(nums, nums.size(), target, 0, 0);
+        int n = nums.size();
+        
+        // (index, sum): count
+        vector<unordered_map<int, int>> mp(n);
+        return DFS(nums, n, 0, target, mp);
     }
     
-    int DFS(vector<int>& nums, int n, int target, int index, int sum){
-        if(index == n) return (sum == target) ? 1 : 0;
+    int DFS(vector<int>& nums, int n, int index, int sum, vector<unordered_map<int, int>>& mp){
+        if(index == n) return (sum == 0);
+        if(mp[index].find(sum) != mp[index].end()) return (mp[index])[sum];
         int count = 0;
-        count += DFS(nums, n, target, index+1, sum+nums[index]);
-        count += DFS(nums, n, target, index+1, sum-nums[index]);
+        count += DFS(nums, n, index+1, sum-nums[index], mp);
+        count += DFS(nums, n, index+1, sum+nums[index], mp);
         
-        return count;
+        return (mp[index])[sum] = count;
     }
 };
