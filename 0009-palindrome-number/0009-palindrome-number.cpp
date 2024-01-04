408ecf01b00b4a3136894e07de1f3adbd49748e8
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-04 09:01:03 +0800

    Time: 3 ms (94.13%), Space: 6.1 MB (98.98%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
+    // stack solution
+    // bool isPalindrome(int x) {
+    //     stack<char> s;
+    //     string str = to_string(x);
+    //     int len = str.size(); bool odd = len%2;
+    //     int i = 0;
+    //     for(; i<len/2; i++) s.push(str[i]);
+        
+    //     if(odd) i++;
+        
+    //     for(; i<len; i++){
+    //         if(str[i] == s.top()) s.pop();
+    //         else return false;
+    //     }
+        
+    //     if(s.empty()) return true;
+    //     else return false;
+    // }
+    
+    // math solution
     bool isPalindrome(int x) {
-        stack<char> s;
-        string str = to_string(x);
-        int len = str.size(); bool odd = len%2;
-        int i = 0;
-        for(; i<len/2; i++) s.push(str[i]);
-        
-        if(odd) i++;
-        
-        for(; i<len; i++){
-            if(str[i] == s.top()) s.pop();
-            else return false;
+        if(x < 0) return false;
+
+        int y = x;
+        long long num = 0;
+        while(y != 0){
+            num = num*10 + y%10;
+            y = y/10;
         }
-        
-        if(s.empty()) return true;
-        else return false;
+
+        return (num == x);
     }
 };
