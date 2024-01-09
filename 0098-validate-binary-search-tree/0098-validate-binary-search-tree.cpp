d4945cc9f69c120fc766b3319fa29184b04d89e5
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-09 09:38:48 +0800

    Time: 11 ms (44.63%), Space: 22.2 MB (32.43%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,23 +11,37 @@
  */
 class Solution {
 public:
-    bool isValidBST(TreeNode* root) {
-        stack<TreeNode*> s;
-        TreeNode* prev = nullptr;
-        if(!root) return true;
-        while(root || !s.empty()){
-            while(root){
-                s.push(root);
-                root = root->left;
-            }
+    // iterative solution
+    // bool isValidBST(TreeNode* root) {
+    //     stack<TreeNode*> s;
+    //     TreeNode* prev = nullptr;
+    //     if(!root) return true;
+    //     while(root || !s.empty()){
+    //         while(root){
+    //             s.push(root);
+    //             root = root->left;
+    //         }
             
-            root = s.top(), s.pop();
+    //         root = s.top(), s.pop();
             
-            if(prev && prev->val >= root->val) return false;
-            prev = root;
-            root = root->right;
-        }
+    //         if(prev && prev->val >= root->val) return false;
+    //         prev = root;
+    //         root = root->right;
+    //     }
         
-        return true;
+    //     return true;
+    // }
+
+    // top-down recursive solution
+    bool isValidBST(TreeNode* root) {
+        return helper(root, LLONG_MAX, LLONG_MIN);
+    }
+
+    bool helper(TreeNode* root, long long upper, long long lower){
+        if(!root) return true;
+        if(root->val >= upper || root->val <= lower) return false;
+        bool t1 = helper(root->left, root->val, lower);
+        bool t2 = helper(root->right, upper, root->val);
+        return t1 && t2;
     }
 };
