31c75e91821975813592bf0d92b6d3822c0ab4a0
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-15 13:48:06 +0800

    Time: 414 ms (91.74%), Space: 163 MB (86.82%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -3,21 +3,20 @@ public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
         vector<vector<int>> answer;
         vector<int> t0, t1;
-        map<int, int> player, lost;
+        unordered_map<int, int> lost;
         
         for(auto &p: matches){
-            if(!player[p[0]]) player[p[0]] = 1;
-            if(!player[p[1]]) player[p[1]] = 1;
-            
-            if(!lost[p[1]]) lost[p[1]] = 1;
-            else lost[p[1]]++;
+            if(!lost[p[0]]) lost[p[0]] = 0;
+            lost[p[1]]++;
         }
         
-        for(auto &it: player){
-            if(lost.find(it.first) == lost.end()) t0.push_back(it.first);
-            else if(lost[it.first] == 1) t1.push_back(it.first);
+        for(auto &p: lost){
+            if(p.second == 0) t0.push_back(p.first);
+            else if(p.second == 1) t1.push_back(p.first);
         }
         
+        sort(t0.begin(), t0.end());
+        sort(t1.begin(), t1.end());
         answer.push_back(t0); answer.push_back(t1);
         return answer;
         
