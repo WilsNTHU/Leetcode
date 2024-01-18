6ec5fc7abbf7170c5cc13cc23615191717d49621
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-18 21:36:01 +0800

    Time: 445 ms (63.40%), Space: 0B (100.00%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -1,3 +1,7 @@
 # Write your MySQL query statement below
 UPDATE `Salary`
-SET sex = (CASE WHEN sex='f' THEN 'm' ELSE 'f' END);
+SET `sex` = 
+CASE
+WHEN `sex` = 'f' THEN 'm'
+WHEN `sex` = 'm' THEN 'f'
+END;
