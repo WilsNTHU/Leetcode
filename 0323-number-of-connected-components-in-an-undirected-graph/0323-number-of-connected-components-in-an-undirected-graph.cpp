8d697e3bf1278152e72ea43ff5d21b8c98ba38c7
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-20 20:25:05 +0800

    Time: 12 ms (90.60%), Space: 15.6 MB (21.54%) - LeetHub

---
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -0,0 +1,52 @@
+class Disjoint {
+    public:
+        Disjoint(int size): root(size), rank(size), count(size){
+            for(int i=0; i<size; i++){
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+    
+        int find(int x){
+            if(x == root[x]) return x;
+            return root[x] = find(root[x]);
+        }
+    
+        void unionSet(int x, int y){
+            int rootX = find(x);
+            int rootY = find(y);
+            if(rootX != rootY){
+                if(rank[rootX] < rank[rootY]) root[rootX] = rootY;
+                else if(rank[rootX] > rank[rootY]) root[rootY] = rootX;
+                else{
+                    root[rootY] = rootX;
+                    rank[rootX]++;
+                }
+                count--;
+            }
+        }
+    
+        bool connected(int x, int y){
+            return find(x) == find(y);
+        }
+    
+        int getCount(){
+            return count;
+        }
+    
+    private:
+        vector<int> rank, root;
+        int count;
+    
+};
+
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        Disjoint s(n);
+        for(auto &it: edges){
+            s.unionSet(it[0], it[1]);
+        }
+        return s.getCount();
+    }
+};
