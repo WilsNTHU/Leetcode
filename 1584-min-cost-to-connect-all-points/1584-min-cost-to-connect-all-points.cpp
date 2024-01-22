46181de2691278600f4c10114cf39619a4be9075
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-22 23:05:30 +0800

    Time: 140 ms (78.92%), Space: 61.1 MB (43.04%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,74 +1,46 @@
-class Disjoint {
+class Edge{
     public:
-        Disjoint(int size): root(size), rank(size), count(size){
-            for(int i=0; i<size; i++){
-                root[i] = i;
-                rank[i] = 1;
-            }
-        }
-
-        int find(int x){
-            if(x == root[x]) return x;
-            return root[x] = find(root[x]);
-        }
-
-        void unionSet(int x, int y){
-            int rootX = find(x);
-            int rootY = find(y);
-            
-            if(rank[rootX] < rank[rootY]) root[rootX] = rootY;
-            else if(rank[rootX] > rank[rootY]) root[rootY] = rootX;
-            else{
-                root[rootY] = rootX;
-                rank[rootX]++;
-            }
-            count--;
-        }
-
-        bool connected(int x, int y){
-            return find(x) == find(y);
-        }
-    
-    private:
-        vector<int> rank, root;
-        int count;
+        int p1, p2, cost;
+        Edge(int _p1, int _p2, int _cost): p1(_p1), p2(_p2), cost(_cost){}
 };
+
+bool operator<(const Edge& e1, const Edge &e2){
+    return e1.cost > e2.cost;
+}
 
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        vector<tuple<int, int, int>> edges;
-        Disjoint ds(n);
-        
-        for(int i=0; i<n; i++)
-            for(int j=i+1; j<n; j++){
-                int dis = abs(points[i][0]-points[j][0]) 
-                         + abs(points[i][1]-points[j][1]);
-                edges.emplace_back(i, j, dis);
-            }
-        
-        sort(edges.begin(), edges.end(), 
-             [](tuple<int, int, int> &t1, tuple<int, int, int> &t2){
-                 int p1, p2, p3, p4, dis1, dis2;
-                 tie(p1, p1, dis1) = t1;
-                 tie(p3, p4, dis2) = t2;
-                 return dis1 < dis2;
-             });
-        
+        // min_heap
+        priority_queue<Edge> pq;
+        vector<bool> vis(n, false);
+        int count = 0;
         int ans = 0;
-        int edge_count = 0;
         
-        for(auto &t: edges){
-            if(edge_count == n-1) return ans; 
-            
-            int p1, p2, dis;
-            tie(p1, p2, dis) = t;
-            if(ds.find(p1) != ds.find(p2)){
-                ds.unionSet(p1,p2);
-                ans += dis;
-                edge_count++;
+        for(int i=1; i<n; i++){
+            int cost = abs(points[i][0]-points[0][0]) + 
+                       abs(points[i][1]-points[0][1]);
+            pq.push(Edge(0, i, cost));
+        }
+        
+        vis[0] = true;
+        while(!pq.empty() && count!=n-1){
+            Edge e = pq.top(); pq.pop();
+            int pt = e.p2;
+            if(!vis[pt]){
+                vis[pt] = true;
+                ans += e.cost;
+                for(int i=0; i<n; i++){
+                    if(!vis[i]){
+                        int cost = abs(points[i][0]-points[pt][0]) + 
+                           abs(points[i][1]-points[pt][1]);
+                        pq.push(Edge(pt, i, cost));
+                    }
+                }
+                count++;
             }
+            
         }
         
         return ans;
