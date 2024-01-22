096c720e1492f461d211e5054c116512124616eb
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-22 08:25:32 +0800

    Time: 15 ms (97.34%), Space: 24.7 MB (34.82%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n = *max_element(nums.begin(), nums.end());
-        unordered_map<int, int> mp(n);
-        vector<int> result;
-        for(auto &x: nums) mp[x]++;
-        
-        for(int i=1; i<=n; i++){
-            if(mp[i]==2)
-                result.push_back(i);
+        int n = nums.size();
+        vector<int> mp(n+1, 0);
+        vector<int> ans(2, -1);
+        for(int i=0; i<n; i++){
+            int num = nums[i];
+            mp[num]++;
+            if(mp[num] == 2) ans[0] = num;
         }
         
-        for(int i=1; i<=n; i++){
-            if(mp[i]==0)
-                result.push_back(i);
-        }
+        for(int i=1; i<=n; i++)
+            if(mp[i] == 0) ans[1] = i;
         
-        if(result.size() == 2) return result;
-        else result.push_back(n+1);
-        
-        return result;
+        return ans;
     }
 };
