71821c954f1647d5e4c4b39dcda9423767349eb4
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-23 16:41:28 +0800

    Time: 10 ms (98.52%), Space: 16.4 MB (14.55%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int, int>>> graph(n);
+        for(auto &t: flights) graph[t[0]].push_back({t[1], t[2]});
+        
         vector<int> dist(n, INT_MAX);
         dist[src] = 0;
-        for(int i=0; i<k+1; i++){ // Bellman-ford, with at most k+1 edges
-            vector<int> temp = dist;
-            for(auto &t: flights){
-                int u=t[0], v=t[1], cost=t[2];
-                if(dist[u] == INT_MAX) continue;
-                if(dist[u] + cost < temp[v])
-                    temp[v] = dist[u] + cost;
+        
+        
+        queue<tuple<int, int, int>> q; // (cost, node, edge_count)
+        q.push({0, src, 0});
+        
+        while(!q.empty()){
+            auto [cost, node, edge_count] = q.front(); q.pop();
+            if(edge_count >= k+1) continue;
+        
+            for(auto &p: graph[node]){
+                int adjNode = p.first, edW = p.second;
+                if(cost + edW < dist[adjNode] && edge_count<=k){
+                    dist[adjNode] = cost + edW;
+                    q.push({dist[adjNode], adjNode, edge_count+1});
+                }
             }
-            dist = temp;
         }
         
-
         if(dist[dst] == INT_MAX) return -1;
         else return dist[dst];
     }
