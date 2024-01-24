d6ccc12b50d7e76acd0cf3eb584c63d174ba8f1c
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-24 15:08:02 +0800

    Time: 7 ms (81.69%), Space: 13 MB (5.43%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -13,13 +13,19 @@ public:
             }
         }
         
+        vector<pair<int, int>> temp;
+        for(int i=0; i<k; i++){
+            temp.push_back(pq.top());
+            pq.pop();
+        }
+        
+        sort(temp.begin(), temp.end(), [](pair<int, int> &p1, pair<int, int> &p2){
+            if(p1.first == p2.first) return p1.second < p2.second;
+            return p1.first < p2.first;
+        });
             
         vector<int> ans;
-        for(int i=0; i<k; i++){
-            ans.push_back(pq.top().second);
-            pq.pop();
-        } 
-        reverse(ans.begin(), ans.end());
+        for(auto &p: temp) ans.push_back(p.second);
         return ans;
     }
 };
