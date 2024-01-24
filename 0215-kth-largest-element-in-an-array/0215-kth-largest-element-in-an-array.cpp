9e7413e921c576476c132735719727aab498b788
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-24 11:12:30 +0800

    Time: 101 ms (57.73%), Space: 59.8 MB (27.26%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        heapSort(nums);
-        vector<int>:: iterator it = nums.begin();
-        advance(it, k-1);
-        return *it;
-    }
-    
-    void heapSort(vector<int>& nums){
         int n = nums.size();
-        for(int i=n/2 - 1; i>=0; i--) minHeapify(nums, n, i);
-        for(int i=n-1; i>0; i--){
-            int temp = nums[0];
-            nums[0] = nums[i];
-            nums[i] = temp;
-            minHeapify(nums, i, 0);
-        } 
+        
+        vector<int> heap;
+        for(int i=0; i<k; i++) heap.push_back(nums[i]);
+        for(int i=k/2-1; i>=0; i--) min_heapify(heap, i, k);
+        
+        for(int i=k; i<n; i++){
+            if(nums[i] > heap[0]){
+                heap[0] = nums[i];
+                min_heapify(heap, 0, k);
+            }
+        }
+        
+        for(int i=k-1; i>0; i--){
+            int temp = heap[i];
+            heap[i] = heap[0];
+            heap[0] = temp;
+            min_heapify(heap, 0, i-1);
+        }
+        
+        return heap[k-1];
     }
     
-    void minHeapify(vector<int>& nums, int n, int idx){
-        int left = 2*idx + 1;
-        int right = 2*idx + 2;
-        int smallest = idx;
-        if(left<n && nums[left]<nums[smallest]) smallest = left;
-        if(right<n && nums[right]<nums[smallest]) smallest = right;
-        
-        if(smallest != idx){
-            int temp = nums[idx];
-            nums[idx] = nums[smallest];
-            nums[smallest] = temp;
-            minHeapify(nums, n, smallest);
+    void min_heapify(vector<int> &heap, int idx, int size){
+        int left_child = 2*idx + 1;
+        int right_child = 2*idx + 2;
+        int largest = idx;
+        if(left_child < size && heap[largest] > heap[left_child]) 
+            largest = left_child;
+        if(right_child < size && heap[largest] > heap[right_child]) 
+            largest = right_child;
+        if(largest != idx){
+            int temp = heap[largest];
+            heap[largest] = heap[idx];
+            heap[idx] = temp;
+            min_heapify(heap, largest, size);
         }
     }
+    
 };
