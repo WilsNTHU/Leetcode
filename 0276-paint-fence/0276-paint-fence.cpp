6af50cee46c2eeded8842880673b0f4a93bd127a
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-27 20:09:16 +0800

    Time: 3 ms (23.32%), Space: 7.3 MB (6.71%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.cpp
+++ 0276-paint-fence/0276-paint-fence.cpp
@@ -9,7 +9,17 @@ public:
     }
     
     int numWays(int n, int k) {
-        vector<int> dp(n+1, -1);
-        return helper(dp, k, n);
+        // vector<int> dp(n+1, -1);
+        if(n == 1) return k;
+        if(n == 2) return k*k;
+        
+        
+        vector<int> dp(n+1, 0);
+        dp[1] = k;
+        dp[2] = k*k;
+        for(int i=3; i<=n; i++) 
+            dp[i] = (dp[i-1] + dp[i-2]) * (k-1);
+        return dp[n];
+        // return helper(dp, k, n);
     }
 };
