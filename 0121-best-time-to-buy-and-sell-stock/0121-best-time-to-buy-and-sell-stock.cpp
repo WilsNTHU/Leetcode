afdddbed895f32c36e6d43118b208020ebf17818
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-28 16:28:11 +0800

    Time: 96 ms (57.75%), Space: 95.9 MB (9.66%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,17 +2,26 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<int> dp(n, -1);
+        // Below is Dynamic Programming Approach
+        // vector<int> dp(n, -1);
         
-        dp[n-1] = prices[n-1];
-        for(int i=n-2; i>=0; i--) dp[i] = max(prices[i], dp[i+1]);
+        // dp[n-1] = prices[n-1];
+        // for(int i=n-2; i>=0; i--) dp[i] = max(prices[i], dp[i+1]);
         
-        int ans = INT_MIN;
-        for(int i=0; i<=n-2; i++){
-            ans = max(ans, -prices[i] + dp[i+1]);
+        // int ans = INT_MIN;
+        // for(int i=0; i<=n-2; i++){
+        //    ans = max(ans, -prices[i] + dp[i+1]);
+        // }
+        
+        // if(ans <= 0) return 0;
+        // return ans;
+        
+        int min_buy = INT_MAX, max_sell = 0;
+        for(int i=0; i<=n-1; i++){
+            min_buy = min(min_buy, prices[i]);
+            max_sell = max(max_sell, prices[i]-min_buy);
         }
         
-        if(ans <= 0) return 0;
-        return ans;
+        return max_sell;
     }
 };
