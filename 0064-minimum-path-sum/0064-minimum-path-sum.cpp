6d812e4b6fd420f31e7456af1af51d61ac76a4f3
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-01-29 13:52:32 +0800

    Time: 8 ms (48.55%), Space: 12.5 MB (12.10%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int helper(vector<vector<int>> &grid, vector<vector<int>> &dp, int i, int j){
         int m = grid.size(), n = grid[0].size();
+        if(i>=m || j>=n) return INT_MAX;
+        
         if(dp[i][j] != -1) return dp[i][j];
         if(i==(m-1) && j==(n-1)) return grid[i][j];
         
-        int right_sum = INT_MAX, down_sum = INT_MAX;
-        if(i < (m-1)) down_sum = helper(grid, dp, i+1, j);
-        if(j < (n-1)) right_sum = helper(grid, dp, i, j+1);
+        int right_sum = helper(grid, dp, i, j+1);
+        int down_sum = helper(grid, dp, i+1, j);
         
         return dp[i][j] = grid[i][j] + min(down_sum, right_sum);
     }
