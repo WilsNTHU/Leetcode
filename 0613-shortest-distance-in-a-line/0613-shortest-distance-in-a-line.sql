fa01a269c4a3559c8d45bcbf5495612a7efd466a
ladies_boy_3333 <136956659+WilsNTHU@users.noreply.github.com>
2024-02-06 14:48:08 +0800

    Time: 557 ms (29.51%), Space: 0B (100.00%) - LeetHub

--- 0613-shortest-distance-in-a-line/0613-shortest-distance-in-a-line.sql
+++ 0613-shortest-distance-in-a-line/0613-shortest-distance-in-a-line.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
-SELECT MIN(ABS(`p1`.`x`-`p2`.`x`)) AS `shortest`
-FROM `Point` `p1`, `Point` `p2`
-WHERE `p1`.`x` != `p2`.`x`;
+# SELECT MIN(ABS(`p1`.`x`-`p2`.`x`)) AS `shortest`
+# FROM `Point` `p1`, `Point` `p2`
+# WHERE `p1`.`x` != `p2`.`x`;
+
+WITH `distances` AS (
+    SELECT ABS(`x` - LEAD(`x`) OVER (ORDER BY `x` ASC)) AS `distance`
+    FROM `Point`
+)
+
+SELECT MIN(`distance`) AS `shortest` FROM `distances`;
